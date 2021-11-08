3bfd2815bd72d99e15054eb8cb20b66672bac1be
Anna <anna.kulakova1991@gmail.com>
2021-11-08 20:16:19 +0300

    fix

--- js/main.js
+++ js/main.js
@@ -2,9 +2,3 @@ import {createPhoto} from './utils/data.js';
 import {renderSimilarPhoto} from './picture/picture.js';
 import './form/form.js';
 import './utils/slider.js';
-
-const SIMILAR_PHOTO_COUNT = 25;
-
-fetch ('https://24.javascript.pages.academy/kekstagram/data')
-  .then((response) => response.json())
-  .then((photo) => renderSimilarPhoto(photo.slice(0, SIMILAR_PHOTO_COUNT)));
